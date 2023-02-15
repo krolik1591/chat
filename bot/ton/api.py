45061058ad13e36738542db26f9fbd455c343439
krolik1591 <dasenkosasa19@gmail.com>
2023-02-15 21:41:01 +0200

    ton WIP

--- bot/ton/api.py
+++ bot/ton/api.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import aiohttp
 
 
@@ -6,13 +8,18 @@ class Api:
         self.api_base = api_base
         self.token = token
 
-    async def get_address_transactions(self, address):
-        # todo
-        return await self._api("getTransactions", {
+
+    async def get_address_transactions(self, address, hash_=None, lt=None):
+        params = {
             "address": address,
             "limit": 30,
-            "archival": True
-        })
+            "archival": 'true',
+        }
+        if hash_ is not None:
+            params['hash'] = hash_
+        if lt is not None:
+            params['lt'] = lt
+        return await self._api("getTransactions", params)
 
     async def _api(self, method, params):
         async with aiohttp.ClientSession() as session:
@@ -20,3 +27,14 @@ class Api:
                 r.raise_for_status()
                 response = await r.json()
                 return response['result']
+
+if __name__ == "__main__":
+    api = Api('https://testnet.toncenter.com/api/v2' ,'621699dde4b908a9d5c98ab16a887e9348ed3a05afe44a32bf8e6244f7a2bde0')
+
+
+    async def test():
+        pprint(await api.get_address_transactions('EQDnV_p8jmDZgj7lFetCctUrEAss8Xdv3x-MZwiZqJHNFnQw'))
+
+
+    import asyncio
+    asyncio.run(test())
--- bot/ton/wallets.py
+++ bot/ton/wallets.py
@@ -1,3 +1,7 @@
+import os
+import pathlib
+import shutil
+
 from ton import TonlibClient
 
 
@@ -6,6 +10,8 @@ class Wallets:
         self.wallet_seed = wallet_seed
         self.client = TonlibClient()
         TonlibClient.enable_unaudited_binaries()
+
+    async def init(self):
         await self.client.init_tonlib()
 
     async def get_wallet(self, wallet_id: int):
@@ -14,3 +20,21 @@ class Wallets:
     async def transfer(self, wallet_id: int, to_address: str):
         pass
         # todo
+
+
+if __name__ == "__main__":
+    wallets = Wallets(wallet_seed="water wish artist boss random burst entry assault size "
+                                 "february equal inner satoshi wire camp reason throw "
+                                 "allow chapter dose gym jungle vibrant truth")
+
+
+    async def test():
+        await wallets.init()
+        wallet = await wallets.get_wallet(1)
+        print(wallet.address)
+
+    import asyncio
+    asyncio.run(test())
+
+    file_to_rem = pathlib.Path("./.keystore")
+    shutil.rmtree(file_to_rem)
