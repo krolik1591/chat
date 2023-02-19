4e4c2165e698d4522be415fb9e1421c7ac2f6bd0
krolik1591 <dasenkosasa19@gmail.com>
2023-02-19 03:00:49 +0200

    transfer WIP

--- bot/ton/wallets.py
+++ bot/ton/wallets.py
@@ -4,11 +4,10 @@ import shutil
 
 from ton import TonlibClient
 
-
 class Wallets:
-    def __init__(self, wallet_seed: str):
+    def __init__(self, wallet_seed: str, config='https://ton-blockchain.github.io/testnet-global.config.json'):
         self.wallet_seed = wallet_seed
-        self.client = TonlibClient()
+        self.client = TonlibClient(config=config)
         TonlibClient.enable_unaudited_binaries()
 
     async def init(self):
@@ -17,9 +16,10 @@ class Wallets:
     async def get_wallet(self, wallet_id: int):
         return await self.client.import_wallet(self.wallet_seed, wallet_id=wallet_id)
 
-    async def transfer(self, wallet_id: int, to_address: str):
-        pass
-        # todo
+    async def transfer(self, wallet_id: int, to_address: str, amount: int):
+        sender_wallet = await self.get_wallet(wallet_id)
+
+        return await sender_wallet.transfer(to_address, amount)
 
 
 if __name__ == "__main__":
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     async def test():
         await wallets.init()
         wallet = await wallets.get_wallet(1)
+        # wallets.transfer(1, 'EQAwmioWn9M2qqbtUPjPFY50-0NENZFL2D5Kr_xu8nG5Qswm', 1000)
         print(wallet.address)
 
     import asyncio
