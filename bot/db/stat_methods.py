f7194aa6025dc836330dbef6c16ab3daebaef4d5
krolik1591 <dasenkosasa19@gmail.com>
2023-02-24 20:22:26 +0200

    add stat_methods

---
+++ bot/db/stat_methods.py
@@ -0,0 +1,32 @@
+from bot.db import first_start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    async def test():
+        await first_start()
+        # await update_user_balance(357108179, 1, 500)
+        # a = await get_token_by_id(2)
+        # print(a)
+        # print(await get_user_balance(4, 1))
+        # x = await get_user_balances(4)
+
+
+    import asyncio
+
+    asyncio.run(test())
