a58983d066dd5fe5c1df58c672ab00c96f52381b
krolik1591 <dasenkosasa19@gmail.com>
2023-02-24 19:02:36 +0200

    fix `Дай гроші`

--- bot/handlers/m03_token.py
+++ bot/handlers/m03_token.py
@@ -1,19 +1,19 @@
 from aiogram import Router, types
 from aiogram.dispatcher.fsm.context import FSMContext
 
-from bot.const import START_POINTS
+from bot.const import START_POINTS, MIN_BET
 from bot.db.methods import update_user_balance, get_user_balance
 from bot.menus import get_game_menu
 
 router = Router()
 
 
-# only for DEMO balance
+# only for DEMO token
 @router.callback_query(text=["end_money"])
-async def choice_main_or_demo_balance(call: types.CallbackQuery, state: FSMContext):
+async def replenish_demo_balance(call: types.CallbackQuery, state: FSMContext):
     await update_user_balance(call.from_user.id, 1, START_POINTS)
     user_data = await state.get_data()
-    user_bet = user_data.get('bet')
+    user_bet = user_data.get('bet', MIN_BET)
     token_icon = user_data.get('token_icon')
     user_balance = await get_user_balance(call.from_user.id, 1)
 
