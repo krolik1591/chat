7ffc29479019204e109c936d024f953073097734
krolik1591 <dasenkosasa19@gmail.com>
2023-02-24 20:22:06 +0200

    fix time replenish demo balance

--- bot/handlers/m03_token.py
+++ bot/handlers/m03_token.py
@@ -1,8 +1,11 @@
+import time
+from datetime import datetime
+
 from aiogram import Router, types
 from aiogram.dispatcher.fsm.context import FSMContext
 
 from bot.const import START_POINTS, MIN_BET
-from bot.db.methods import update_user_balance, get_user_balance
+from bot.db.methods import update_user_balance, get_user_balance, add_new_transaction
 from bot.menus import get_game_menu
 
 router = Router()
@@ -11,7 +14,8 @@ router = Router()
 # only for DEMO token
 @router.callback_query(text=["end_money"])
 async def replenish_demo_balance(call: types.CallbackQuery, state: FSMContext):
-    await update_user_balance(call.from_user.id, 1, START_POINTS)
+    DEMO_TOKEN = 1
+    await update_user_balance(call.from_user.id, DEMO_TOKEN, START_POINTS)
     user_data = await state.get_data()
     user_bet = user_data.get('bet', MIN_BET)
     token_icon = user_data.get('token_icon')
@@ -19,5 +23,7 @@ async def replenish_demo_balance(call: types.CallbackQuery, state: FSMContext):
 
     text, keyboard = get_game_menu(user_bet, user_balance, token_icon)
     await call.message.edit_text(text, reply_markup=keyboard)
+
+    await add_new_transaction(call.from_user.id, DEMO_TOKEN, 0, int(time.time()), START_POINTS, 'demo_address', 'demo_hash')
 
 # todo
