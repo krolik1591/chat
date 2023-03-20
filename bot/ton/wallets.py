257c33cb5e0c799e9e25c188add5e092917e23ee
svin <svinerus@gmail.com>
2023-03-20 05:12:47 +0200

    Delete methods for blocks txs

--- bot/ton/wallets.py
+++ bot/ton/wallets.py
@@ -1,18 +1,10 @@
 import pathlib
-from collections import defaultdict
-from pprint import pprint
 
 from TonTools import *
 from ton.tl.functions import Raw_GetTransactions
 from ton.tl.types import Internal_TransactionId
-from tonsdk.utils import b64str_to_hex
-
-from bot.ton.tl import BlockLookup, Blocks_GetTransaction, Blocks_AccountTransactionId, Blocks_GetMasterchainInfo
 
 libpath = pathlib.Path(__file__).parent / 'libtonlibjson.so.0.5'
-
-# master workchain; master shard
-workchain, shard = -1, -9223372036854775808
 
 
 class TonWrapper(LsClient):
@@ -96,47 +88,8 @@ class TonWrapper(LsClient):
 
         return results
 
-    # block tx
-
-    async def get_block_transactions(self, block):
-        return await self._get_block_transactions(workchain, shard, block)
-
-    async def _get_block_transactions(self, workchain, shard, seqno):
-
-        fullblock = await self._lookup_block(workchain, shard, seqno)
-        after_tx = Blocks_AccountTransactionId()
-        total_result = []
-
-        while True:
-            result = await self._raw_get_block_transactions(fullblock, 256, after_tx)
-            total_result += result.transactions
-
-            if not result.incomplete:
-                break
-
-            after_tx = Blocks_AccountTransactionId(result.transactions[-1].account, result.transactions[-1].lt)
-
-        for tx in total_result:
-            tx.account = f"{workchain:d}:{b64str_to_hex(tx.account)}"
-
-        return total_result
-
-    async def _raw_get_block_transactions(self, fullblock, count, after_tx):
-        request = Blocks_GetTransaction(fullblock, count, after_tx)
-        return await self.tonlib_wrapper.execute(request, timeout=self.default_timeout)
-
-    async def _lookup_block(self, workchain, shard, seqno=None, lt=None, unixtime=None):
-        request = BlockLookup(workchain, shard, seqno, lt, unixtime)
-        return await self.tonlib_wrapper.execute(request, timeout=self.default_timeout)
-
-    # utils
-
-    async def get_masterchain_info(self):
-        return await self.tonlib_wrapper.execute(Blocks_GetMasterchainInfo(), timeout=self.default_timeout)
-
 
 if __name__ == '__main__':
-
     async def test():
         ton_wrapper = await TonWrapper.create()
 
@@ -146,65 +99,4 @@ if __name__ == '__main__':
         await ton_wrapper.tonlib_wrapper.close()
 
 
-    async def main():
-        BLOCK_TIME = 5  # seconds
-        MAX_BLOCKS_AT_TIME = 100
-
-        ton_wrapper = await TonWrapper.create()
-        last_processed_block = 100
-
-        while True:
-            last_block_number = (await ton_wrapper.get_masterchain_info()).last.seqno
-
-            block_from = last_processed_block
-            block_to = min(block_from+MAX_BLOCKS_AT_TIME, last_block_number)
-
-            print(f'processing block {block_from=} {block_to=}')
-            blocks_txs = await get_txs_from_blocks(ton_wrapper, block_from, block_to)
-            print('find', len(blocks_txs), 'txs')
-            addresses = await get_addresses_in_txs(ton_wrapper, blocks_txs)
-            print(addresses)
-
-            last_processed_block = block_to
-
-            await asyncio.sleep(BLOCK_TIME)
-
-        await ton_wrapper.tonlib_wrapper.close()
-
-
-    async def get_txs_from_blocks(ton_wrapper, block_from, block_to):
-        blocks_txs = []
-        for block_num in range(block_from, block_to):
-            block_txs = await ton_wrapper.get_block_transactions(block_num)
-            blocks_txs.extend(block_txs)
-        return blocks_txs
-
-    async def get_addresses_in_txs(ton_wrapper, block_txs):
-        results = set()  # hashset (unique values)
-
-        addresses = defaultdict(dict)
-        for tx in block_txs:
-            addr = addresses[tx.account]
-            if 'newest_lt' not in addr or int(tx.lt) > addr['newest_lt']:
-                addr['newest_lt'] = int(tx.lt)
-                addr['newest_hash'] = tx.hash
-            if 'oldest_lt' not in addr or int(tx.lt) < addr['oldest_lt']:
-                addr['oldest_lt'] = int(tx.lt)
-
-        print(len(addresses), 'uniq callers in txs')
-
-        for addr, options in addresses.items():
-            norm_addr = Address(addr).to_string(True, False, True, False)
-            txs = await ton_wrapper.get_account_transactions(
-                norm_addr,
-                last_tx_hash=options['newest_hash'], last_tx_lt=options['newest_lt'],
-                first_tx_lt=options['oldest_lt'], include_first_tx=True
-            )
-            for tx in txs:
-                results.add(tx['source'])
-                results.add(tx['destination'])
-
-        return results
-
-
-    asyncio.run(main())
+    asyncio.run(test())
