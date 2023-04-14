2381e1a44b766cc2ddae7a8d21236887a19f35c6
krolik1591 <dasenkosasa19@gmail.com>
2023-04-14 19:19:45 +0300

    reformat `withdraw_menu_err`

--- bot/menus/deposit_menus/withdraw_menu/withdraw_menu_err.py
+++ bot/menus/deposit_menus/withdraw_menu/withdraw_menu_err.py
@@ -3,23 +3,18 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from bot.texts import WITHDRAW_ERR1, WITHDRAW_ERR2, \
     WITHDRAW_ERR3, WITHDRAW_ERR4, WITHDRAW_ERR5, WITHDRAW_ERR6
 
+WITHDRAW_ERR = {
+    1: WITHDRAW_ERR1,   # round_user_withdraw < MIN_WITHDRAW
+    2: WITHDRAW_ERR2,   # user balance < MIN_WITHDRAW
+    3: WITHDRAW_ERR3,   # test.net address
+    4: WITHDRAW_ERR4,   # incorrect address
+    5: WITHDRAW_ERR5,   # round_user_withdraw > user_balance
+    6: WITHDRAW_ERR6,   # end money on master wallet
+}
+
 
 def withdraw_menu_err(err):
-    if err == 1:
-        text = WITHDRAW_ERR1.format()   # round_user_withdraw < MIN_WITHDRAW
-    elif err == 2:
-        text = WITHDRAW_ERR2.format()   # user balance < MIN_WITHDRAW
-    elif err == 3:
-        text = WITHDRAW_ERR3.format()   # test.net address
-    elif err == 4:
-        text = WITHDRAW_ERR4.format()   # incorrect address
-    elif err == 5:
-        text = WITHDRAW_ERR5.format()   # round_user_withdraw > user_balance
-    elif err == 6:
-        text = WITHDRAW_ERR6.format()   # end money on master wallet
-
-
-
+    text = WITHDRAW_ERR[err]
     kb = _keyboard()
 
     return text, kb
