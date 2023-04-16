be6dd9cc18e6e1f5b3703297849dda6b91e739b0
krolik1591 <dasenkosasa19@gmail.com>
2023-04-16 15:32:53 +0300

    fix incorrect display withdraw ton_amount

--- bot/menus/deposit_menus/withdraw_menu/withdraw_menu3.py
+++ bot/menus/deposit_menus/withdraw_menu/withdraw_menu3.py
@@ -4,9 +4,10 @@ from bot.texts import WITHDRAW_MENU_TEXT3
 
 
 def withdraw_menu_check(user_withdraw_amount, user_withdraw_address, token_price):
-    user_withdraw_amount_ton = user_withdraw_amount / token_price
+    user_withdraw_amount_ton = round(user_withdraw_amount / token_price, 4)
     text = WITHDRAW_MENU_TEXT3.format(user_withdraw_amount=user_withdraw_amount,
-                                      user_withdraw_address=user_withdraw_address, user_withdraw_amount_ton=user_withdraw_amount_ton)
+                                      user_withdraw_address=user_withdraw_address,
+                                      user_withdraw_amount_ton=user_withdraw_amount_ton)
     kb = _keyboard()
 
     return text, kb
