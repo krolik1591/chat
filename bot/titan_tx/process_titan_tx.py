82da49c30543a9a2066b6e1a02d9845275595984
krolik1591 <dasenkosasa19@gmail.com>
2023-04-17 16:53:36 +0300

    add process err if `admin_chat_id` incorrect

--- bot/titan_tx/process_titan_tx.py
+++ bot/titan_tx/process_titan_tx.py
@@ -1,17 +1,26 @@
+import logging
 import time
 
+from aiogram.exceptions import TelegramMigrateToChat
+
+from bot.db.db import manager
 from bot.db.methods import add_new_titan_tx, update_user_balance
 from bot.menus.deposit_menus.withdraw_menu.withdraw_titan_tx_menu import process_titan_tx_menu
 from bot.utils.config_reader import config
 
 
 async def process_titan_tx(user_id, username, ton_amount, context, token, user_withdraw_address):
-    new_tx = await add_new_titan_tx(user_id=user_id, token_id=token.token_id, nano_ton_amount=ton_amount * 10**9,
-                                    tx_address=user_withdraw_address, utime=int(time.time()))
-    await update_user_balance(user_id, token.token_id, -ton_amount * token.price)
+    with manager.pw_database.atomic():
+        new_tx = await add_new_titan_tx(user_id=user_id, nano_ton_amount=ton_amount * 10**9, token_id=token.token_id,
+                                        price=token.price, tx_address=user_withdraw_address, utime=int(time.time()))
+        id_new_tx = new_tx.titanTXs_id
 
-    id_new_tx = new_tx.titanTXs_id
+        text, keyboard = process_titan_tx_menu(user_id, username, ton_amount, id_new_tx)
+        try:
+            await context.fsm_context.bot.send_message(
+                chat_id=config.admin_chat_id, text=text, reply_markup=keyboard)
+        except TelegramMigrateToChat as ex:
+            logging.exception('User trying withdraw cash')
+            return
 
-    text, keyboard = process_titan_tx_menu(user_id, username, ton_amount, id_new_tx, token.token_id, token.price)
-    await context.fsm_context.bot.send_message(
-        chat_id=config.admin_chat_id, text=text, reply_markup=keyboard)
+        await update_user_balance(user_id, token.token_id, -ton_amount * token.price)
