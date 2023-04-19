06ae00fc8f5f960973bed38d160080921dca24e0
krolik1591 <dasenkosasa19@gmail.com>
2023-04-20 00:54:18 +0300

    fix daily total amount

--- bot/handlers/deposit_handlers/d02_withdraw.py
+++ bot/handlers/deposit_handlers/d02_withdraw.py
@@ -124,7 +124,7 @@ async def approve_withdraw(call: types.CallbackQuery, state: FSMContext):
     if correct_withdraw_tx is False:
         return
 
-    if user_withdraw_amount > MAXIMUM_WITHDRAW:
+    if ton_amount > MAXIMUM_WITHDRAW:
         context = await Context.from_fsm_context(call.from_user.id, state)
         await process_manual_tx(call.from_user.id, call.from_user.username, ton_amount, context, token, user_withdraw_address)
         return
@@ -145,7 +145,6 @@ async def check_user_input_amount(message, context):
     token_id = context.state.get(StateKeys.TOKEN_ID)
     token = await get_token_by_id(token_id)
     user_balance = await get_user_balance(message.from_user.id, token_id)
-    ton_amount = user_withdraw / token.price
 
     if round_user_withdraw < MIN_WITHDRAW:
         text, keyboard = withdraw_menu_err.withdraw_too_small(token_price=token.price)
@@ -157,29 +156,15 @@ async def check_user_input_amount(message, context):
         await message.answer(text, reply_markup=keyboard)
         return
 
-    # if ton_amount > MAXIMUM_WITHDRAW:
-    #
-    #
-    #     # text, keyboard = withdraw_menu_err.withdraw_too_big(token.price)
-    #     # await message.answer(text, reply_markup=keyboard)
-    #     return
-
     return round_user_withdraw
 
 
 async def check_unresolved_manual_tx(call, state, token, user_withdraw_amount, user_withdraw_address):
     user_daily_total_amount_nano_ton = await get_user_daily_total_amount(call.from_user.id)
     total_amount_price = user_daily_total_amount_nano_ton / 10**9 * token.price
-    ton_amount = user_withdraw_amount / token.price
 
     if total_amount_price + user_withdraw_amount > MAXIMUM_WITHDRAW_DAILY * token.price:
         text, keyboard = withdraw_menu_err.reached_daily_limit(MAXIMUM_WITHDRAW_DAILY*token.price - total_amount_price)
         await state.bot.send_message(call.from_user.id, text, reply_markup=keyboard)
-
-        await add_new_manual_tx(user_id=call.from_user.id, nano_ton_amount=ton_amount * 10**9, token_id=token.token_id,
-                                price=token.price, tx_address=user_withdraw_address,
-                                utime=int(time.time()), withdraw_state='rejected')
         return False
     return True
-
-
