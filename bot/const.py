cf9d3042a365436c9b1fd686ccca41cb13cc3dc5
krolik1591 <dasenkosasa19@gmail.com>
2023-04-19 23:58:53 +0300

    fix emoji

--- bot/const.py
+++ bot/const.py
@@ -5,8 +5,8 @@ START_POINTS = 500
 
 MIN_WITHDRAW = 50   # tokens
 INIT_PAY_TON = 0.015    # TON
-MAXIMUM_WITHDRAW = 1    # TON
-MAXIMUM_WITHDRAW_DAILY = 4   # TON
+MAXIMUM_WITHDRAW = 0.55    # TON
+MAXIMUM_WITHDRAW_DAILY = 1.1   # TON
 
 THROTTLE_TIME_SPIN = 2  # время искусственной задержки между броском дайса и ответом, оно же период троттлинга
 THROTTLE_TIME_OTHER = 1  # время искусственной задержки между остальными командами, оно же период троттлинга
