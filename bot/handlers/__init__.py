541af13ee22f77aa5ab79bf8032f6fc283362c05
svin <svinerus@gmail.com>
2023-04-20 19:23:16 +0300

    Rename `deposit_handlers` -> `balance_handlers`

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,4 +1,4 @@
-from .deposit_handlers import routers as balance_routers
+from .balance_handlers import routers as balance_routers
 from .games_handlers import routers as game_routers
 from .m01_main import router as m01_main_router
 from .other_handlers import routers as other_routers
