86316dd35b7311bc68a0fea6197870214b5556dd
svin <svinerus@gmail.com>
2023-04-20 20:35:10 +0300

    Rename files in `wallet_handlers`

--- bot/handlers/wallet_handlers/__init__.py
+++ bot/handlers/wallet_handlers/__init__.py
@@ -1,5 +1,5 @@
-from .d01_deposit import router as d01_replenish_router
-from .d02_withdraw import router as d02_withdraw_router
-from .d03_manual_withdraw import router as d03_manual_withdraw_router
+from .deposit import router as d01_replenish_router
+from .withdraw import router as d02_withdraw_router
+from .withdraw_manual import router as d03_manual_withdraw_router
 
 routers = [d01_replenish_router, d02_withdraw_router, d03_manual_withdraw_router]
