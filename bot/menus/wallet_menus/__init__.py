c75894cd56e482b3ac197239f0f2f6d5a0a29ce1
svin <svinerus@gmail.com>
2023-04-20 21:00:33 +0300

    Fix export

--- bot/menus/wallet_menus/__init__.py
+++ bot/menus/wallet_menus/__init__.py
@@ -1,3 +1,2 @@
 from .wallet_menu import wallet_menu
-from .deposit_menu import deposit_menu
-from . import withdraw_menu, withdraw_menu_err
+from . import deposit_menu, withdraw_menu, withdraw_menu_err
