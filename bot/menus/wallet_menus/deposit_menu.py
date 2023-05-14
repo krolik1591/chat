71e9da7a9033edeb2c80255d7daeb9902c984877
svin <svinerus@gmail.com>
2023-05-14 21:41:16 +0300

    Update `deposit_menu.py` to use i18n

--- bot/menus/wallet_menus/deposit_menu.py
+++ bot/menus/wallet_menus/deposit_menu.py
@@ -1,40 +1,40 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.i18n import gettext as _
 
 from bot.consts.const import TON_INITIALISATION_FEE
 from bot.menus.utils import kb_del_msg
-from bot.consts.texts import DEPOSIT_INITIATION_ERROR, DEPOSIT_ACCOUNT_INITIATED, DEPOSIT_SUCCESSFUL, \
-    DEPOSIT_MENU_TEXT
 from bot.utils.rounding import round_down
 
 
 def deposit_account_initiation(is_successful_inited):
     if is_successful_inited:
-        text = DEPOSIT_ACCOUNT_INITIATED.format(init_pay_ton=TON_INITIALISATION_FEE)
+        text = _('DEPOSIT_ACCOUNT_INITIATED').format(init_pay_ton=TON_INITIALISATION_FEE)
     else:
-        text = DEPOSIT_INITIATION_ERROR.format(init_pay_ton=TON_INITIALISATION_FEE)
+        text = _('DEPOSIT_INITIATION_ERROR').format(init_pay_ton=TON_INITIALISATION_FEE)
 
     return text, kb_del_msg()
 
 
 def successful_deposit_menu(amount):
     amount = round_down(amount, 2)
-    text = DEPOSIT_SUCCESSFUL.format(amount=amount)
+    text = _('DEPOSIT_SUCCESSFUL').format(amount=amount)
     return text, kb_del_msg()
 
 
 def deposit_menu(wallet_address):
-    text = DEPOSIT_MENU_TEXT.format(wallet_address=wallet_address)
+    text = _('DEPOSIT_MENU_TEXT').format(wallet_address=wallet_address)
     kb = _replenish_menu_keyboard(wallet_address)
 
     return text, kb
 
 
+
 def _replenish_menu_keyboard(wallet_address):
     kb = [
-        [InlineKeyboardButton(text='Відкрити Tonkeeper', url=f"https://app.tonkeeper.com/transfer/{wallet_address}")],
-        [InlineKeyboardButton(text='Todo link', url=f"https://app.tonkeeper.com/transfer/{wallet_address}")],
+        [InlineKeyboardButton(text=_('DEPOSIT_MENU_BTN_OPEN_TONKEEPER'), url=f"https://app.tonkeeper.com/transfer/{wallet_address}")],
+        [InlineKeyboardButton(text=_('DEPOSIT_MENU_BTN_OPEN_TONKEEPER2'), url=f"https://app.tonkeeper.com/transfer/{wallet_address}")],
         [
-            InlineKeyboardButton(text='‹ Мій гаманець', callback_data="wallet_menu")
+            InlineKeyboardButton(text=_('DEPOSIT_MENU_BTN_BACK'), callback_data="wallet_menu")
         ]
     ]
 
