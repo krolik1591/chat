7b0e2d27f23a385db71fae4424872884f535e3a6
krolik1591 <dasenkosasa19@gmail.com>
2023-06-07 15:13:40 +0300

    add more WOF menus

--- bot/handlers/wheel_of_fortune_handlers/wheel_of_fortune.py
+++ bot/handlers/wheel_of_fortune_handlers/wheel_of_fortune.py
@@ -2,7 +2,8 @@ from aiogram import Router, types
 from aiogram.filters import Text
 from aiogram.fsm.context import FSMContext
 
-from bot.menus.main_menus.wheel_of_fortune_menus import wheel_of_fortune_menu
+from bot.menus.main_menus.wheel_of_fortune_menus import buy_random_num_menu, buy_selected_num_menu, buy_ticket_menu, \
+    wheel_of_fortune_menu
 
 router = Router()
 
@@ -11,4 +12,25 @@ router = Router()
 async def wheel_of_fortune(call: types.CallbackQuery, state: FSMContext):
 
     text, keyboard = wheel_of_fortune_menu()
+    await call.message.edit_text(text, reply_markup=keyboard)
+
+
+@router.callback_query(Text("buy_ticket"))
+async def buy_ticket(call: types.CallbackQuery, state: FSMContext):
+
+    text, keyboard = buy_ticket_menu()
+    await call.message.edit_text(text, reply_markup=keyboard)
+
+
+@router.callback_query(Text("buy_selected_num"))
+async def buy_selected_ticket(call: types.CallbackQuery, state: FSMContext):
+
+    text, keyboard = buy_selected_num_menu()
+    await call.message.edit_text(text, reply_markup=keyboard)
+
+
+@router.callback_query(Text("buy_random_num"))
+async def buy_selected_ticket(call: types.CallbackQuery, state: FSMContext):
+
+    text, keyboard = buy_random_num_menu()
     await call.message.edit_text(text, reply_markup=keyboard)
--- bot/menus/main_menus/wheel_of_fortune_menus.py
+++ bot/menus/main_menus/wheel_of_fortune_menus.py
@@ -10,7 +10,38 @@ def wheel_of_fortune_menu():
         [InlineKeyboardButton(text=_('WHEEL_FORTUNE_BTN_BUY_TICKET_CHECK_MY_STATUS'), callback_data="check_status")],
         [InlineKeyboardButton(text=_('WHEEL_FORTUNE_BTN_SPIN_RESULT'), callback_data="spin_result")],
         [InlineKeyboardButton(text=_('WHEEL_FORTUNE_BTN_CLAIM_WIN'), callback_data="claim_win")],
-        [InlineKeyboardButton(text=_('BTN_BACK'), callback_data="list_for_spam_approved")],
+        [InlineKeyboardButton(text=_('BTN_BACK'), callback_data="main_menu")],
+    ])
+
+    return text, kb
+
+
+def buy_ticket_menu():
+    text = _('WOF_MENU_BUY_TICKET_TEXT')
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=_('WOF_BTN_BUY_TICKET_MENU_SELECT_NUM'), callback_data="buy_selected_num")],
+        [InlineKeyboardButton(text=_('WOF_BTN_BUY_TICKET_MENU_RANDOM_NUM'), callback_data="buy_random_num")],
+        [InlineKeyboardButton(text=_('BTN_BACK'), callback_data="wheel_of_fortune")],
+    ])
+
+    return text, kb
+
+
+def buy_selected_num_menu():
+    text = _('WOF_MENU_BUY_SELECTED_TICKET_TEXT')
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=_('WOF_BTN_BUY_SELECTED_NUM_MENU'), callback_data="buy_this_num")],
+        [InlineKeyboardButton(text=_('BTN_BACK'), callback_data="buy_ticket")],
+    ])
+
+    return text, kb
+
+
+def buy_random_num_menu():
+    text = _('WOF_MENU_BUY_SELECTED_TICKET_TEXT')
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=_('WOF_BTN_BUY_RANDOM_NUM_MENU'), callback_data="buy_this_num")],
+        [InlineKeyboardButton(text=_('BTN_BACK'), callback_data="buy_ticket")],
     ])
 
     return text, kb
