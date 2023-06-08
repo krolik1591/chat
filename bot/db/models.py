535173c56b75ed2ade26b01f60715770fdce6219
krolik1591 <dasenkosasa19@gmail.com>
2023-06-08 17:41:00 +0300

    add table `wofsettings` rename `wheeloffortune` to `woftickets`

--- bot/db/models.py
+++ bot/db/models.py
@@ -39,12 +39,23 @@ class User(manager.Model):
         return f'USER: {self.user_id}; {self.lang=}'
 
 
-class WheelOfFortune(manager.Model):
-    wheeloffortune_id = BigIntegerField(primary_key=True)
+class WoFTickets(manager.Model):
+    woftickets_id = BigIntegerField(primary_key=True)
     user = ForeignKeyField(User, backref='WheelOfFortune')
 
     ticket_num = BigIntegerField()
     timestamp_buy_last_ticket = DateTimeField()
+
+
+class WoFSettings(manager.Model):
+    wofsettings_id = BigIntegerField(primary_key=True)
+
+    ticket_cost = BigIntegerField()
+    commission = BigIntegerField()
+    who_win = CharField()
+
+    timestamp_start = DateTimeField()
+    timestamp_end = DateTimeField(null=True)
 
 
 class WithdrawTx(manager.Model):
@@ -90,4 +101,4 @@ class GameLog(manager.Model):
 
 class Settings(manager.Model):
     key = CharField(primary_key=True)
-    value = CharField()
+    value = CharField()     # json format
