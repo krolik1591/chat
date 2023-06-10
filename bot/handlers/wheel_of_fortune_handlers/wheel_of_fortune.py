9beeec14d5b2519ffb2d19e5c14fb131124c5301
krolik1591 <dasenkosasa19@gmail.com>
2023-06-10 14:17:02 +0300

    add locale_datetime to `check_status_menu` and `my_numbers_menu`

--- bot/handlers/wheel_of_fortune_handlers/wheel_of_fortune.py
+++ bot/handlers/wheel_of_fortune_handlers/wheel_of_fortune.py
@@ -1,6 +1,10 @@
+import datetime
+
 from aiogram import Router, types
 from aiogram.filters import Text
 from aiogram.fsm.context import FSMContext
+from aiogram.utils.i18n import gettext as _
+from babel.dates import format_datetime
 
 from bot.db import db
 from bot.menus.main_menus.wheel_of_fortune_menus import wheel_of_fortune_doesnt_exist_menu, \
@@ -20,6 +24,23 @@ async def wheel_of_fortune(call: types.CallbackQuery, state: FSMContext):
         return
 
     user_tickets = await db.get_count_user_tickets(call.from_user.id, 'all')
+    locale_datetime = await get_locale_datetime(state, wof_info.timestamp_end)
 
-    text, keyboard = wheel_of_fortune_menu(wof_info.ticket_cost, wof_info.timestamp_end, user_tickets, user_wof_win)
+    text, keyboard = wheel_of_fortune_menu(wof_info.ticket_cost, locale_datetime, user_tickets, user_wof_win)
     await call.message.edit_text(text, reply_markup=keyboard)
+
+
+@router.callback_query(Text("check_status"))
+async def check_status_menu(call: types.CallbackQuery, state: FSMContext):
+    wof_info = await db.get_wheel_info()
+    locale_datetime = await get_locale_datetime(state, wof_info.timestamp_end)
+    await call.answer(_('WOF_CHECK_STATUS_ANSWER').format(date_end=locale_datetime), show_alert=True)
+
+
+async def get_locale_datetime(state, date_end):
+    locale = (await state.get_data()).get('locale')
+    date_end = date_end
+    if isinstance(date_end, int):
+        timestamp = 1686411432000 / 1000  # Ділимо на 1000, оскільки ми працюємо з мілісекундами
+        date_end = datetime.datetime.fromtimestamp(timestamp)
+    return format_datetime(date_end, format='medium', locale=locale)
