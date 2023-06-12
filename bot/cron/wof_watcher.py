e70cc108398a8456f35d7c99d1da1493c146db6b
svin <svinerus@gmail.com>
2023-06-12 20:23:35 +0300

    Refactor wof_watcher a little

--- bot/cron/wof_watcher.py
+++ bot/cron/wof_watcher.py
@@ -1,10 +1,11 @@
 import asyncio
 import logging
 import time
-from datetime import datetime
 
 from bot.db import db
 from bot.handlers.wheel_of_fortune_handlers.wof_start_event import start_wheel_of_fortune
+
+HOUR = 3600
 
 
 async def start_wof_timer():
@@ -13,21 +14,14 @@ async def start_wof_timer():
         wof_info = await db.get_active_wheel_info()
 
         if not wof_info:
-            await asyncio.sleep(3600)
+            await asyncio.sleep(HOUR)
             logging.info("WOF TIMER SLEEP")
             continue
 
-        try:
-            date_end = wof_info.timestamp_end
-        except AttributeError:
-            logging.info("Date end not found")
-            await asyncio.sleep(3600)
-            continue
-        now = time.time()
-        time_delta = date_end - now
-        logging.info(f"Seconds to WOF: {time_delta}")
-        if time_delta < 3600:
+        time_before_wof_finish = wof_info.timestamp_end - time.time()
+        logging.info(f"Seconds to WOF: {time_before_wof_finish}")
+        if time_before_wof_finish < HOUR:
             logging.info("Timer to WOF is started")
             await start_wheel_of_fortune()
 
-        await asyncio.sleep(3600)
+        await asyncio.sleep(HOUR)
