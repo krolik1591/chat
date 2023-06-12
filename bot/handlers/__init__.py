f462008e324db9ab9d7143e6355758c75eab2c33
svin <svinerus@gmail.com>
2023-06-12 21:24:06 +0300

    Fix typo

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,7 @@
 from .games_handlers import routers as game_routers
-from .guides_heandlers import routers as guides_routers
+from .guides_handlers import routers as guides_routers
 from .m01_main import router as m01_main_router
-from .my_account_heandlers import routers as my_account_routers
+from .my_account_handlers import routers as my_account_routers
 from .other_handlers import routers as other_routers
 from .wallet_handlers import routers as balance_routers
 from .admin_handlers import routers as admin_routers
