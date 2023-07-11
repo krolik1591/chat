78a4804b0ec08bad31460da5997effa181a33749
krolik1591 <dasenkosasa19@gmail.com>
2023-07-11 15:23:49 +0300

    add `promo_codes_menu`

--- bot/handlers/my_account_handlers/my_account.py
+++ bot/handlers/my_account_handlers/my_account.py
@@ -2,7 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import Text
 
 from bot.db import db
-from bot.menus.account_menus.account_menu import my_account_menu
+from bot.menus.account_menus.account_menu import my_account_menu, promo_code_menu
 
 router = Router()
 
@@ -12,4 +12,11 @@ async def settings(call: types.CallbackQuery):
     balances = await db.get_user_balances(call.from_user.id)
 
     text, keyboard = my_account_menu(balances)
+    await call.message.edit_text(text, reply_markup=keyboard)
+
+
+@router.callback_query(Text("promo_codes"))
+async def promo_codes(call: types.CallbackQuery):
+
+    text, keyboard = promo_code_menu()
     await call.message.edit_text(text, reply_markup=keyboard)
--- bot/menus/account_menus/account_menu.py
+++ bot/menus/account_menus/account_menu.py
@@ -32,3 +32,13 @@ def _keyboard():
     ]
 
     return InlineKeyboardMarkup(inline_keyboard=kb)
+
+
+def promo_code_menu():
+    text = _('ACCOUNT_MENU_PROMO_CODES_TEXT')
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=_("ACCOUNT_MENU_ACTIVE_PROMO_CODES_BTN"), callback_data="active_promo_codes")],
+        [InlineKeyboardButton(text=_("BTN_BACK"), callback_data="my_account")]
+    ])
+
+    return text, kb
