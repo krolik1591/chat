f4342beb206dd2f45510688353f7c826ff3f3e06
krolik1591 <dasenkosasa19@gmail.com>
2023-07-13 19:14:49 +0300

    display promo code info in `my_promo_code_menu`

--- bot/menus/account_menus/promocodes_menu.py
+++ bot/menus/account_menus/promocodes_menu.py
@@ -23,10 +23,20 @@ def active_promo_code_menu(text):
     return text, kb
 
 
-def my_promo_code_menu():
-    text = _('PROMOCODES_MENU_MY_PROMO_CODES_TEXT')
+def my_promo_code_menu(sum_bets, min_wager, wager, active_promo):
+    if sum_bets is None:
+        sum_bets = 0
+
+    text1 = f'{sum_bets}/{min_wager}'
+    text2 = f'{sum_bets}/{wager}'
+
+    if min_wager is None or wager is None:
+        text1 = _("PROMOCODES_MENU_NEED_DEPOSIT_TEXT")
+        text2 = text1
+
+    text = _('PROMOCODES_MENU_MY_PROMO_CODES_TEXT').format(text1=text1, text2=text2, active_promo=active_promo)
     kb = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=_("PROMOCODES_MENU_CLAIM_REWARD"), callback_data="claim_reward")],
+        [InlineKeyboardButton(text=_("PROMOCODES_MENU_CLAIM_REWARD_BTN"), callback_data="claim_reward")],
         [InlineKeyboardButton(text=_("BTN_BACK"), callback_data="promo_codes")]
     ])
 
