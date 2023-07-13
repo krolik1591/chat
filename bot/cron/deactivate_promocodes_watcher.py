038368a95ff0d06ca7c54ffa470a9d6efdfcb75a
krolik1591 <dasenkosasa19@gmail.com>
2023-07-13 21:50:14 +0300

    create file `deactivate_promocodes_watcher`

---
+++ bot/cron/deactivate_promocodes_watcher.py
@@ -0,0 +1,31 @@
+import logging
+from datetime import datetime, timedelta
+from bot.db import db
+
+import pytz
+
+HOUR = 3600
+
+
+async def start_wof_timer(bot, i18n):
+    while True:
+        logging.info("DEACTIVATE PROMO CODE TIMER STARTED")
+
+        await db.get_all_active_promo_code_for_user()
+
+        await asyncio.sleep(HOUR)
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    async def test():
+        gmt_timezone = pytz.timezone('Etc/GMT')
+        current_datetime = datetime.now(gmt_timezone)
+        next_day = current_datetime + timedelta(days=1)
+        next_day = next_day.replace(hour=0, minute=0, second=0, microsecond=0)
+        unix_timestamp = next_day.timestamp()
+        print(unix_timestamp)
+
+
+    asyncio.run(test())
