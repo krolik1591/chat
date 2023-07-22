d6e86eb4fade1ab24ee1b8c9903df3ec4f6cc9e5
krolik1591 <dasenkosasa19@gmail.com>
2023-07-22 12:37:29 +0300

    rename

--- bot/cron/warning_about_end_promo_watcher.py
+++ bot/cron/warning_about_end_promo_watcher.py
@@ -23,7 +23,7 @@ async def warning_about_expiration_promo_code(bot, i18n):
             continue
 
         for user_id in users:
-            promo_code = await db.get_all_info_user_promo_code(user_id, 'balance')
+            promo_code = await db.get_all_info_user_promo_code_by_type(user_id, 'balance')
             hours, minutes = convert_seconds_to_hours_minutes(promo_code.date_end - time.time())
 
             await set_user_locale_to_i18n(user_id, i18n)
