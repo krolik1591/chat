51f00c3012e7a179e3fa7202457f90a5f5fc289f
krolik1591 <dasenkosasa19@gmail.com>
2023-07-27 16:28:03 +0300

    add monitoring promo bonus in `crypto_bot_tx_watcher`

--- bot/cron/crypto_bot_tx_watcher.py
+++ bot/cron/crypto_bot_tx_watcher.py
@@ -3,7 +3,7 @@ import logging
 from pprint import pprint
 
 from bot.consts.const import USDT_TO_GAMETOKENS
-from bot.db import db
+from bot.db import db, manager
 from bot.tokens.CryptoPay import CryptoPay
 from bot.tokens.token_ton.tx_watcher import send_successful_deposit_msg, set_user_locale_to_i18n
 from bot.tokens.tokens import TOKENS
@@ -28,19 +28,28 @@ async def process_invoice(bot, i18n, invoice):
     logging.info(f'Process invoice {invoice.invoice_id} in crypto bot tx watcher start')
     user_id = int(invoice.payload)
     token_id = invoice.asset.lower()
-    await db.add_new_crypto_pay_tx(user_id, token_id, invoice.amount * 10 ** 9, invoice.invoice_id, invoice.paid_at.timestamp())
 
     if token_id not in TOKENS:
         logging.info(f"Crypto pay token {token_id} not in TOKENS")
         return
 
     token = TOKENS[token_id]
-    gametokens = await token.to_gametokens(invoice.amount)
-    await db.update_user_balance(user_id, 'general', round_down(gametokens, 5))
+    amount_gametokens = await token.to_gametokens(invoice.amount)
+    promo_code = await db.need_a_bonus(user_id)
+
+    with manager.pw_database.atomic():
+        if promo_code:
+            promo_bonus = amount_gametokens * promo_code.promocode.bonus / 100
+            await db.update_user_balance(user_id, 'promo', promo_bonus)
+            await db.update_wagers_and_bonus(user_id, promo_bonus, promo_code)
+
+        await db.update_user_balance(user_id, 'general', round_down(amount_gametokens, 5))
+        await db.add_new_crypto_pay_tx(user_id, token_id, invoice.amount * 10 ** 9, invoice.invoice_id,
+                                       invoice.paid_at.timestamp())
 
     try:
         await set_user_locale_to_i18n(user_id, i18n)
-        await send_successful_deposit_msg(bot, user_id, round_down(gametokens, 2))
+        await send_successful_deposit_msg(bot, user_id, round_down(amount_gametokens, 2))
     except ValueError:
         print('User from crypto bot tx watcher not found in db')
 
