fe70685e6b0dcfc68cea9c071f27dac84eef8fe0
krolik1591 <dasenkosasa19@gmail.com>
2023-08-06 01:09:30 +0300

    fix

--- bot/handlers/wallet_handlers/deposit.py
+++ bot/handlers/wallet_handlers/deposit.py
@@ -1,11 +1,10 @@
-from TonTools.Contracts.Wallet import Wallet
 from aiogram import Router, types
 from aiogram.filters import Text
 from aiogram.fsm.context import FSMContext
 
 from bot.db import db
+from bot.handlers.states import StateKeys
 from bot.menus.wallet_menus.deposit_menu import deposit_menu
-from bot.tokens.token_ton import TonWrapper
 
 router = Router()
 
@@ -13,6 +12,8 @@ router = Router()
 @router.callback_query(Text("replenish"))
 async def replenish(call: types.CallbackQuery, state: FSMContext):
     user_wallet = await db.get_user_wallet(call.from_user.id)
+
+    await state.update_data({StateKeys.ENTERED_DEPOSIT_AMOUNT: None})
 
     text, keyboard = deposit_menu(user_wallet.address)
     await call.message.edit_text(text, reply_markup=keyboard)
